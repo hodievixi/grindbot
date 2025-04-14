317bd16afd9197b82ec09370fe547e895180588e
Arkhi <odematro1@gmail.com>
2025-04-14 21:38:00 +0200

    🧼 Updated bot to simplified info version

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 # === Загрузка .env ===
 load_dotenv()
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("7748501866:AAHqQx1nxgJ2Lc2j_iKJhbFAFTY7c4AtFjk")
 bot = telebot.TeleBot(TOKEN)
 
 # === Главное меню ===
